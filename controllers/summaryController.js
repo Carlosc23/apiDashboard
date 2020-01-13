f98fd22e673e1901def2beb58a10df2acdccf93b
Carlosc23 <cal15219@uvg.edu.gt>
2020-01-13 01:29:23 -0600

    Update summaryController.js

--- controllers/summaryController.js
+++ controllers/summaryController.js
@@ -5,23 +5,23 @@ const Client = require("../models/Client");
 async function retrieveSummary(req, res, next) {
 
       const clientCount = await Client.estimatedDocumentCount({}).distinct('documento');
-      /*const clientAmount = await Client.aggregate([{
+      const clientAmount = await Client.aggregate([{
         $group:{
             _id:'',
             "mnt_trx_mm":{$sum:'$mnt_trx_mm'}
         }
     }]
-      );*/
-      /*const clientTransaction =  await Client.aggregate([{
+      );
+      const clientTransaction =  await Client.aggregate([{
         $group:{
             _id:'',
             "num_trx":{$sum:'$num_trx'}
         }
-    }]);*/
+    }]);
     res.json({
         clients: clientCount.length,
-        //amount: clientAmount[0].mnt_trx_mm,
-        //transaction: clientTransaction[0].num_trx
+        amount: clientAmount[0].mnt_trx_mm,
+        transaction: clientTransaction[0].num_trx
     })
  
   }
